5a91c274539b01ea285f5841ca5dce5c83ebb02a
davydovbv <53862456+davydovbv@users.noreply.github.com>
2021-03-17 11:19:07 +0300

    solution: Done Task2 for theme "Intro"

---
+++ Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+    public static void main (String [] args) {
+       String a, b;
+       a = "Java: version 11.";
+       b = "JVM: HotSpot.";
+        System.out.println (a + "\n" + b);
+    }
+}
